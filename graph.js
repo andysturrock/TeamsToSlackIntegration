d8928949df215325d74436339b4e33b0a0f098bc
Andy Sturrock <github@sturrock.org>
2019-07-27 14:24:15 +0100

    Can list teams and channels

---
+++ graph.js
@@ -0,0 +1,64 @@
+var graph = require('@microsoft/microsoft-graph-client');
+var util = require('util')
+
+module.exports = {
+  getUserDetails: async function (accessToken) {
+    const client = getAuthenticatedClient(accessToken);
+
+    const user = await client.api('/me').get();
+    return user;
+  },
+
+  getTeamsAndChannels: async function (accessToken) {
+    const client = getAuthenticatedClient(accessToken);
+
+    try {
+      var teamsAndChannels = []
+      const teams = await getMyTeams(client)
+      for (team of teams.value) {
+        channels = await getChannelsInTeam(client, team.id)
+        teamsAndChannels.push({ id: `${team.id}`, displayName: `${team.displayName}`, channels: channels.value })
+      }
+      return teamsAndChannels;
+    }
+    catch (error) {
+      console.log("Error: " + util.inspect(error))
+    }
+  }
+};
+
+function getAuthenticatedClient(accessToken) {
+  // Initialize Graph client
+  const client = graph.Client.init({
+    // Use the provided access token to authenticate
+    // requests
+    authProvider: (done) => {
+      done(null, accessToken);
+    }
+  });
+
+  return client;
+}
+
+async function getMyTeams(client) {
+  const teams = await client
+    .api('/me/joinedTeams')
+    .version('beta')
+    .select('id,displayname')
+    .get();
+
+  return teams;
+}
+
+async function getChannelsInTeam(client, team_id) {
+
+  const channels = await client
+    .api(`/teams/${team_id}/channels`)
+    .version('beta')
+    .select('id,displayname')
+    .get();
+
+  return channels;
+}
+
+
---
+++ routes/teams.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var tokens = require('../tokens.js');
+var graph = require('../graph.js');
+var util = require('util')
+
+/* GET /calendar */
+router.get('/',
+  async function(req, res) {
+    if (!req.isAuthenticated()) {
+      // Redirect unauthenticated requests to home page
+      res.redirect('/')
+    } else {
+      let params = {
+        active: { calendar: true }
+      };
+
+      // Get the access token
+      var accessToken;
+      try {
+        accessToken = await tokens.getAccessToken(req);
+      } catch (err) {
+        req.flash('error_msg', {
+          message: 'Could not get access token. Try signing out and signing in again.',
+          debug: JSON.stringify(err)
+        });
+      }
+
+      if (accessToken && accessToken.length > 0) {
+        try {
+          // Get the events
+          var teamsAndChannels = await graph.getTeamsAndChannels(accessToken);
+          console.log("teamsAndChannels:" + util.inspect(teamsAndChannels))
+          params.teamsAndChannels = teamsAndChannels;
+        } catch (err) {
+          req.flash('error_msg', {
+            message: 'Could not fetch teams',
+            debug: JSON.stringify(err)
+          });
+        }
+      }
+
+      res.render('teams', params);
+    }
+  }
+);
+
+module.exports = router;

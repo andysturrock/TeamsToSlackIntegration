e5164247ade6c84cafa123833fbcbfa01cfa34a5
Andy Sturrock <github@sturrock.org>
2019-08-14 18:27:31 +0100

    Give up on axios, just use promise around https module

--- server/oauth/tokens.js
+++ server/oauth/tokens.js
@@ -2,7 +2,6 @@
 const util = require('util')
 const querystring = require('qs');
 const https = require('https');
-const axios = require('axios');
 const logger = require('pino')()
 
 module.exports = {
@@ -42,7 +41,7 @@ module.exports = {
   },
 
   getOnBehalfOfTokenAsync: async function (accessToken) {
-    try {
+    return new Promise((resolve, reject) => {
 
       const postData = querystring.stringify({
         'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
@@ -52,57 +51,34 @@ module.exports = {
         'scope': `${process.env.OAUTH_GRAPH_SCOPES}`,
         'requested_token_use': 'on_behalf_of'
       });
-      const config = {
+
+      const options = {
+        hostname: 'login.microsoftonline.com',
+        method: 'POST',
+        path: `/${process.env.OAUTH_TENANT_ID}/oauth2/v2.0/token`,
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
           'Content-Length': postData.length
         }
       };
-      const response = await axios.post(`https://login.microsoftonline.com/${process.env.OAUTH_TENANT_ID}/oauth2/v2.0/token`, postData, config)
 
-      logger.error("getOnBehalfOfToken() response" + util.inspect(response));
+      var req = https.request(options, (res) => {
+        res.on('data', (data) => {
+          const response = JSON.parse(new String(data))
+          if(response.error) {
+            reject(response)
+          }
+          resolve(response)
+        });
+      });
 
-      // const postData = querystring.stringify({
-      //   'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-      //   'client_id': process.env.OAUTH_APP_ID,
-      //   'client_secret': process.env.OAUTH_APP_PASSWORD,
-      //   'assertion': accessToken,
-      //   'scope': `${process.env.OAUTH_GRAPH_SCOPES}`,
-      //   'requested_token_use': 'on_behalf_of'
-      // });
-      // logger.error("getOnBehalfOfToken() accessToken:" + util.inspect(accessToken));
-      // logger.error("getOnBehalfOfToken() postData:" + util.inspect(postData));
+      req.on('error', (error) => {
+        reject(error)
+      });
 
-      // const options = {
-      //   hostname: 'login.microsoftonline.com',
-      //   method: 'POST',
-      //   path: `/${process.env.OAUTH_TENANT_ID}/oauth2/v2.0/token`,
-      //   headers: {
-      //        'Content-Type': 'application/x-www-form-urlencoded',
-      //        'Content-Length': postData.length
-      //      }
-      // };
-      // logger.error("getOnBehalfOfToken() options" + util.inspect(options));
-
-      // var req = https.request(options, (res) => {
-      //   logger.error('statusCode:', res.statusCode);
-      //   logger.error('headers:', res.headers);
-
-      //   res.on('data', (d) => {
-      //     process.stdout.write("Wahey: " + d);
-      //   });
-      // });
-
-      // req.on('error', (e) => {
-      //   logger.error(e);
-      // });
-
-      // req.write(postData);
-      // req.end();
-
-    } catch (error) {
-      logger.error('getOnBehalfOfToken() error = ' + util.inspect(error));
-    }
+      req.write(postData);
+      req.end();
+    })
   }
 
 };
